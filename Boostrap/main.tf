9fcd6b642c5ac686aa57cbebf508f751a4724a13
Kacper <kacperzimmer@outlook.com>
2026-03-11 14:19:41 +0100

    bootstrap layer outputs

--- Boostrap/main.tf
+++ Boostrap/main.tf
@@ -3,11 +3,11 @@ provider "aws" {
 }
 
 
-resource "aws_s3_bucket" "state_bucket" {
+resource "aws_s3_bucket" "terraform_state_bucket" {
   bucket = var.bucket_name
 }
 resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
-  bucket = aws_s3_bucket.state_bucket.id
+  bucket = aws_s3_bucket.terraform_state_bucket.id
   versioning_configuration {
     status = "true"
   }
