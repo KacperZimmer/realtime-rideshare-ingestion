b96b31817e059be679058b8d5565b86bcf41895e
Kacper <kacperzimmer@outlook.com>
2026-03-14 13:56:34 +0100

    Changed subnet cidr block to match architecture

--- Networking/main.tf
+++ Networking/main.tf
@@ -10,7 +10,7 @@ resource "aws_vpc" "main" {
 }
 resource "aws_subnet" "private_subnet" {
   vpc_id = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = "10.0.0.0/24"
   availability_zone = var.availability_zone
   tags = {
 
@@ -20,7 +20,7 @@ resource "aws_subnet" "public_subnet" {
   vpc_id = aws_vpc.main.id
   map_public_ip_on_launch = true
   availability_zone = var.availability_zone
-  cidr_block = "10.0.0.0/24"
+  cidr_block = "10.0.1.0/24"
 
   tags = {
 
