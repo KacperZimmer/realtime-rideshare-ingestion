56b65a8b0baada273c59e72c06e409e1b2e29843
Kacper <kacperzimmer@outlook.com>
2026-03-14 13:49:52 +0100

    done

---
+++ Networking/main.tf
@@ -0,0 +1,97 @@
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+
+  tags = {
+
+  }
+}
+resource "aws_subnet" "private_subnet" {
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = var.availability_zone
+  tags = {
+
+  }
+}
+resource "aws_subnet" "public_subnet" {
+  vpc_id = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  availability_zone = var.availability_zone
+  cidr_block = "10.0.0.0/24"
+
+  tags = {
+
+  }
+}
+
+resource "aws_internet_gateway" "gw_1" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+
+  }
+}
+
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet.id
+  depends_on    = [aws_internet_gateway.gw_1]
+
+  tags = {
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw_1.id
+  }
+
+  tags = {
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+  }
+}
+
+resource "aws_route_table_association" "private" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private.id
+}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id       = aws_vpc.main.id
+  service_name = "com.amazonaws.${var.aws_region}.s3"
+
+  route_table_ids = [aws_route_table.private.id]
+
+  tags = {
+  }
+}
---
+++ Networking/variables.tf
@@ -0,0 +1,8 @@
+variable "availability_zone" {
+  type = string
+  default = "eu-west-1a"
+}
+variable "aws_region" {
+  type = string
+  default = "eu-west-1"
+}
